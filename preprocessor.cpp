746f2a5f3abf83c5c04f31f94eff30f1325c694a
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-05-04 18:07:06 +0530

    Add files via upload

---
+++ preprocessor.cpp
@@ -0,0 +1,8 @@
+
+#define minimum(a,b) (a<b)?(a = a):(a=b)
+#define maximum(a,b) (a>b)?(a = a):(a=b)
+#define FUNCTION(a,b) int a;
+#define foreach(v,i) for(int i = 0;i<v.size();i++)
+#define io(v) cin>>v
+#define toStr(str) #str
+#define INF (unsigned)!((int)0)
